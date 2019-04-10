579f84feaeebdae7a5a612862f1636323cc1d03a
longforus <longforus@126.com>
2019-04-10 17:39:28 +0800

    Revert "优化上传到maven"
    This reverts commit b81ca52ae1aa2b90cc4e3ef280b6c90ea3b43001.

--- app/src/main/java/com/chad/baserecyclerviewadapterhelper/SectionMultipleItemUseActivity.java
+++ app/src/main/java/com/chad/baserecyclerviewadapterhelper/SectionMultipleItemUseActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
+
 import com.chad.baserecyclerviewadapterhelper.adapter.SectionMultipleItemAdapter;
 import com.chad.baserecyclerviewadapterhelper.base.BaseActivity;
 import com.chad.baserecyclerviewadapterhelper.data.DataServer;
 import com.chad.baserecyclerviewadapterhelper.entity.SectionMultipleItem;
 import com.chad.library.adapter.base.BaseQuickAdapter;
+
 import java.util.List;
 
 /**
@@ -35,9 +37,10 @@ public class SectionMultipleItemUseActivity extends BaseActivity {
 
         // create adapter which extend BaseSectionMultiItemQuickAdapter provide your headerResId
         SectionMultipleItemAdapter sectionAdapter = new SectionMultipleItemAdapter(R.layout.def_section_head, mData);
-        sectionAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener<SectionMultipleItem>() {
+        sectionAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
             @Override
-            public void onItemChildClick(SectionMultipleItem item,View view,int position) {
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                SectionMultipleItem item = (SectionMultipleItem) adapter.getData().get(position);
                 switch (view.getId()) {
                     case R.id.card_view:
                         // 获取主体item相应数据给后期使用
